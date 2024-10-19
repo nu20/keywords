582db0bb984212c59bff9971d27ca3583196292d
nu20 <bibhutipatra123@gmail.com>
2024-10-19 18:39:11 +0530

    first commit

---
+++ dfj.py
@@ -0,0 +1,9 @@
+def add(a , b) :
+    return a + b
+sum = add(5 , 6)
+print(sum)
+
+def sub (a , b) :
+    return a - b
+subtract = sub(6,5)
+print(subtract)
---
+++ ffwed.py
@@ -0,0 +1,5 @@
+num = int(input("enter the number : "))
+for i in range( 1 , num ) :
+    if i == 6 or i == 9 :
+        continue
+    print(i)
